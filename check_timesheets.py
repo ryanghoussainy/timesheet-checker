d2834e762043d92ee4918127c6f72d5d5c672e42
William <william@ghoussainy.com>
2025-07-10 19:32:44 +0100

    feat: Sum of all hours in every center (haven't done events though). Set header row as the one starting with 'Date'

--- check_timesheets.py
+++ check_timesheets.py
@@ -16,16 +16,33 @@ def read_timesheet(file_path: str) -> list[tuple[str, str, str, str]]:
     Read a timesheet excel file and return a list of tuples of
        (number of hours, start time, end time, rate).
     """
-    df = pd.read_excel(file_path, header=7)
+    
+    # Read the excel file
+    df = pd.read_excel(file_path, header=0)
+    
+    # Get the row number whose first column is 'Date'
+    header_row_index = df[df.iloc[:, 0] == 'Date'].index[0]
+
+    # From df, get all rows after the header row, including the header row
+    table_df = df.iloc[header_row_index:]
+    
+    # Reset the index of the dataframe
+    table_df.reset_index(drop=True, inplace=True)
+    table_df.columns = table_df.iloc[0]
+    table_df = table_df[1:]
 
     # Remove all rows where the Date, Week day, start time, end time are Nan
-    df.dropna(subset=['Date', 'Week', 'Start', 'End'], inplace=True)
-
+    table_df.dropna(subset=['Date', 'Week', 'Start', 'End'], inplace=True)
+    table_df.reset_index(drop=True, inplace=True)
+    print(table_df)
+    
     # Create a tuple of (number of hours, start time, end time, rate)
     timesheet_data = []
-    for _, row in df.iterrows():
+    location_list = ["Masters", "NP", "Chiswick", "Sh. Bush", "Acton", "Water", "Horsenden ", "St Helen's", "Disability", "Squad", "Admin", "Safeguarding "]
+    for _, row in table_df.iterrows():
+        location_hours = [row[loc] if pd.notna(row[loc]) else 0 for loc in location_list]
         timesheet_data.append((
-            row['Acton'],
+            sum(location_hours),
             normalise_time(row['Start']),
             normalise_time(row['End']),
             row['Rate of pay']
