2a8d4ab9945b24c3b28bdb75f7272d00554acbc7
Ryan Ghoussainy <ryan@ghoussainy.com>
2025-09-08 15:21:44 +0100

    fix: error message

--- discrepancies/discrepancy_types/discrepancy.py
+++ discrepancies/discrepancy_types/discrepancy.py
@@ -1,3 +1,3 @@
 class Discrepancy:
     def __str__(self):
-        raise NotImplementedError("Subclasses of Discrepancy must implement __repr__")
+        raise NotImplementedError("Subclasses of Discrepancy must implement __str__")
